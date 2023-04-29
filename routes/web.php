6d7a16b23b3e9782db29a64e988daf00ecc9d3c6
KrzysiekPietrzak <Krzysztof.pietrzak17@gmail.com>
2023-04-29 09:06:04 +0200

    added mail verification

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,37 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
-
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [WelcomeController::class, 'index']);
 
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::resource('products', ProductController::class);
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index')->middleware('auth');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store')->middleware('auth');
-Route::get('/products/edit/{product}', [ProductController::class, 'edit'])->name('products.edit')->middleware('auth');
-Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware('auth');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('auth');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show')->middleware('auth');
+    Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('auth');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+//Route::get('/products', [ProductController::class, 'index'])->name('products.index')->middleware('auth');
+//Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');
+//Route::post('/products', [ProductController::class, 'store'])->name('products.store')->middleware('auth');
+//Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show')->middleware('auth');
+//Route::get('/products/edit/{product}', [ProductController::class, 'edit'])->name('products.edit')->middleware('auth');
+//Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware('auth');
+//Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('auth');
 
-
-
-Route::get('/users/list',[UserController::class,'index']) -> middleware('auth');
-Route::delete('/users/{user}',[UserController::class,'destroy']) -> middleware('auth');
 Route::get('/hello', [HelloWorldController::class, 'show']);
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes(['verify' => true]);
